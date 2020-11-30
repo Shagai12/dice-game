98c5106cd5a51051b0b5c3a2b04f12a82bc0733c
Shagai <shagai555e@gmail.com>
2020-11-30 14:08:21 +0800

    shoogoo sanamsargui baidlaar olj zurgiig gargasan

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var activePlayer = 1;
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 
-
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 
@@ -12,14 +11,24 @@ var roundScore = 0;
 var dice = Math.floor( Math.random() * 6 ) + 1;
 
 // Программ ажиллаж эхэллээ
-document.querySelector('#score-0').textContent = 0;
 
-document.querySelector('#score-1').textContent = 0;
+document.getElementById("score-0").textContent = '0';
 
-document.querySelector('#current-0').textContent = 0;
+document.getElementById('score-1').textContent = '0';
 
-document.querySelector('#current-1').textContent = 0;
+document.getElementById('current-0').textContent = '0';
 
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('current-1').textContent = '0';
 
-console.log('шоо : ' + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+
+    var diceNumber = Math.floor( Math.random() * 6 ) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+});
+
+
